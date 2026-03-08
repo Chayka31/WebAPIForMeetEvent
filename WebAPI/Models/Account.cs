2b45218067da24bc949cd6fb3353148461721bab
Chayka31 <chaechka1337@gmail.com>
2026-03-08 10:34:52 +0300

    124124

--- WebAPI/Models/Account.cs
+++ WebAPI/Models/Account.cs
@@ -7,14 +7,13 @@ namespace WebAPI.Models
     {
 
         public long id { get; set; }
-
         public DateTime created_at { get; set; }
-        public string login { get; set; }
-        public string password { get; set; }
-        public string f_name { get; set; }
-        public string s_name { get; set; }
-        public string m_name { get; set; }
-        public string about { get; set; }
+        public string? login { get; set; }
+        public string? password { get; set; }
+        public string? f_name { get; set; }
+        public string? s_name { get; set; }
+        public string? m_name { get; set; }
+        public string? about { get; set; }
         public byte[]? image { get; set; }
         public string? email { get; set; }
         public long? count_value { get; set; }
